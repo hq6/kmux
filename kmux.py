ff7f0bc14319324d8f76f6ea44e8c294ff9ae26f
Henry Qin <root@hq6.me>
2020-12-21 00:16:51 -0800

    Invoked smux to play around.

--- kmux.py
+++ kmux.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 
 from kubernetes import client, config
+import smux
 
 config.load_kube_config()
 current_namespace = config.list_kube_config_contexts()[1]['context']['namespace']
-print(current_namespace)
+current_context = config.list_kube_config_contexts()[1]['name']
 
 v1 = client.CoreV1Api()
-print("Listing pods with their IPs:")
-ret = v1.list_namespaced_pod(current_namespace)
-for i in ret.items:
-    print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+pods = v1.list_namespaced_pod(current_namespace)
+pod_names = [x.metadata.name for x in pods.items]
+
+pod_commands = [[
+    f'POD={pod_name}',
+    f'KUBE_CONTEXT={current_context}',
+    f'KUBE_NAMESPACE={current_namespace}',
+    'echo $POD $KUBE_CONTEXT $KUBE_NAMESPACE'] for pod_name in pod_names]
+
+smux.create(len(pod_commands), pod_commands, executeBeforeAttach=lambda : smux.tcmd("setw synchronize-panes on"))
