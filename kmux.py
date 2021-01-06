25fffe43f451bc19cec24881d27e23aae4747bde
Henry Qin <root@hq6.me>
2021-01-05 19:12:57 -0800

    Add --deployment option.
    This commit adds an option to select pods by deployment. When both
    deployment and pod_name_regex are given, both are applied as filters.
    
    It also fixes a bug where namespace was being set to current_namespace
    instead of KUBE_NAMESPACE when selecting pods.

--- kmux.py
+++ kmux.py
@@ -24,6 +24,11 @@ def main():
      environment they are running in, should they want to. In this section
      only, lines that start with `#` and blank lines are treated as comments
      and ignored.
+
+  If --deployment and --pod_name_regex are both provided, kmux will select pods
+  that are both in the given deployment and match the given regex.
+
+  If --pods is given, then both --deployment and --pod_name_regex are ignored.
   """
   parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawTextHelpFormatter)
   parser.add_argument('--pods', '-p', metavar='PODS', type=str,
@@ -32,6 +37,8 @@ def main():
                       metavar='POD_NAME_REGEX',
                       type=re.compile,
                       help='Regular expression matching a pod in the namespace.')
+  parser.add_argument('--deployment', '-d', metavar='DEPLOYMENT', type=str,
+                      help='Deployment to select pods from. Defaults to all pods when omitted.')
   parser.add_argument('--kube_context', '-k',
                       metavar='KUBE_CONTEXT',
                       type=str,
@@ -40,7 +47,7 @@ def main():
                       help='Do not create new tmux windows and panes. Run the ' +
                       'commands in only the first found pod in the current ' +
                       'window. One pane will be created if kmux is not started inside a tmux.')
-  parser.add_argument('--dry_run', '-d', action='store_true',
+  parser.add_argument('--dry_run', action='store_true',
                       help='Do not run any tmux commands. Instead, write an smux file to stdout.')
   parser.add_argument('commands_file', nargs='?',
                       type=argparse.FileType('r'),
@@ -69,7 +76,7 @@ def main():
   # input is one or zero lines by removing it.
   if commands[-1] == '':
     commands = commands[:-1]
-  ################################################################################ 
+  ################################################################################
   # Load K8 config and Initialize magic variables
   config.load_kube_config()
   contexts = config.list_kube_config_contexts()[0]
@@ -96,19 +103,37 @@ def main():
   if options.pods:
     PODS = options.pods.split()
   else:
+    api_client = config.new_client_from_config(context=KUBE_CONTEXT)
     # Collect all the pods in the context.
-    kubeClient = client.CoreV1Api(
-        api_client=config.new_client_from_config(context=KUBE_CONTEXT))
-    podObjects = kubeClient.list_namespaced_pod(current_namespace)
-    PODS = [x.metadata.name for x in podObjects.items]
+    core_kube_client = client.CoreV1Api(api_client=api_client)
+    podObjects = core_kube_client.list_namespaced_pod(KUBE_NAMESPACE).items
+
+    # Filter by deployment if given.
+    # 1. Find all ReplicaSets that are owned by the target deployment.
+    # 2. Find all pods owned by the target replica set or replica sets.
+    #
+    # Note that this relies only on owner_references and could be optimized by
+    # using the deployment selector to pre-filter.
+    # However, hqin decided to hold off on this until someone complains about
+    # having such a huge number of pods in a namespace that enumerating them
+    # all would be substantially slow down kmux.
+    if options.deployment:
+        apps_kube_client = client.AppsV1Api(api_client=api_client)
+        replica_sets = apps_kube_client.list_namespaced_replica_set(KUBE_NAMESPACE).items
+        replica_sets = [x for x in replica_sets if not x.spec.replicas == 0 and \
+            x.metadata.owner_references[0].name == options.deployment]
+        replica_set_names = [x.metadata.name for x in replica_sets]
+        podObjects = [pod for pod in podObjects if pod.metadata.owner_references[0].name in replica_set_names]
 
     # Filter by regex if given
     if options.pod_name_regex:
-      PODS = [pod for pod in PODS if options.pod_name_regex.match(pod)]
+      podObjects = [pod for pod in podObjects if options.pod_name_regex.match(pod.metadata.name)]
+
+    PODS = [pod.metadata.name for pod in podObjects]
   if not PODS:
     print("No pods selected.")
     return
-  ################################################################################ 
+  ################################################################################
   pod_commands = [[
       f'POD={POD}',
       f'KUBE_CONTEXT={KUBE_CONTEXT}',
